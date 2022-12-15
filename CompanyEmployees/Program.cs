98103be3f516ec677b4230fa662cdc010c87537d
Ahmed Hisham <ahmed57d@gmail.com>
2022-12-15 03:44:09 +0200

    exception handler

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,4 +1,5 @@
 using CompanyEmployees.Extensions;
+using Contracts;
 using Microsoft.AspNetCore.HttpOverrides;
 using NLog;
 var builder = WebApplication.CreateBuilder(args);
@@ -22,11 +23,16 @@ builder.Services.AddAutoMapper(typeof(Program));
 
 builder.Services.AddControllers().AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
 var app = builder.Build();
+var logger = app.Services.GetRequiredService<ILoggerManager>();
+app.ConfigureExceptionHandler(logger);
+if (app.Environment.IsProduction())
+    app.UseHsts();
 
-if (app.Environment.IsDevelopment())
+
+/*if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 else
-    app.UseHsts();
+    app.UseHsts();*/
 
 // Configure the HTTP request pipeline.
 
