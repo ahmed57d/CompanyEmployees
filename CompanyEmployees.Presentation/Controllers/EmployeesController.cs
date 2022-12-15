f25996838cbb2f2c758336412295fc2018a3336b
Ahmed Hisham <ahmed57d@gmail.com>
2022-12-15 19:17:48 +0200

    getting one employee only and exception handling

--- CompanyEmployees.Presentation/Controllers/EmployeesController.cs
+++ CompanyEmployees.Presentation/Controllers/EmployeesController.cs
@@ -22,5 +22,13 @@ namespace CompanyEmployees.Presentation.Controllers
             false);
             return Ok(employees);
         }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetEmployeeForCompany(Guid companyId, Guid id)
+        {
+            var employee = _service.EmployeeService.GetEmployee(companyId, id,trackChanges: false);
+            return Ok(employee);
+        }
+
     }
-}
+ }
