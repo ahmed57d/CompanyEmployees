6ec1d07b69ac591b053670fadcabe4eb45e2e60c
Ahmed Hisham <ahmed57d@gmail.com>
2022-12-15 19:41:47 +0200

    adding xml output

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -21,7 +21,10 @@ builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.AddAutoMapper(typeof(Program));
 
 
-builder.Services.AddControllers().AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
+builder.Services.AddControllers(config => {
+    config.RespectBrowserAcceptHeader = true;
+}).AddXmlDataContractSerializerFormatters()
+.AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
 var app = builder.Build();
 var logger = app.Services.GetRequiredService<ILoggerManager>();
 app.ConfigureExceptionHandler(logger);
