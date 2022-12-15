8db608979b959f274b2497b4772a5a547ee185f7
Ahmed Hisham <ahmed57d@gmail.com>
2022-12-15 19:59:13 +0200

    Restricting the server from requesting other formats

--- CompanyEmployees/MappingProfile.cs
+++ CompanyEmployees/MappingProfile.cs
@@ -6,14 +6,15 @@ namespace CompanyEmployees
 {
     public class MappingProfile : Profile
     {
-       
+
         public MappingProfile()
         {
             CreateMap<Company, CompanyDto>()
-            .ForCtorParam("FullAddress",
+            .ForMember(c => c.FullAddress,
             opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
             CreateMap<Employee, EmployeeDto>();
         }
+
 
     }
 }
