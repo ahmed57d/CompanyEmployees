a0076a07b636769ae529f909487da2090df1d38a
Ahmed Hisham <ahmed57d@gmail.com>
2022-12-19 20:42:05 +0200

    update company with inserting

--- CompanyEmployees/MappingProfile.cs
+++ CompanyEmployees/MappingProfile.cs
@@ -16,7 +16,7 @@ namespace CompanyEmployees
             CreateMap<CompanyForCreationDto, Company>();
             CreateMap<EmployeeForCreationDto, Employee>();
             CreateMap<EmployeeForUpdateDto, Employee>();
-
+            CreateMap<CompanyForUpdateDto, Company>();
         }
 
 
