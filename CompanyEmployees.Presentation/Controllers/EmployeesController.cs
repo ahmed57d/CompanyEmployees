0f0833c87060486efe563419cb56707a949d8922
Ahmed Hisham <ahmed57d@gmail.com>
2022-12-19 20:10:58 +0200

    update employee

--- CompanyEmployees.Presentation/Controllers/EmployeesController.cs
+++ CompanyEmployees.Presentation/Controllers/EmployeesController.cs
@@ -59,5 +59,18 @@ EmployeeForCreationDto employee)
         }
 
 
+        [HttpPut("{id:guid}")]
+        public IActionResult UpdateEmployeeForCompany(Guid companyId, Guid id,
+[FromBody] EmployeeForUpdateDto employee)
+        {
+            if (employee is null)
+                return BadRequest("EmployeeForUpdateDto object is null");
+            _service.EmployeeService.UpdateEmployeeForCompany(companyId, id, employee,
+            compTrackChanges: false, empTrackChanges: true);
+            return NoContent();
+        }
+
+
+
     }
 }
